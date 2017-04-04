0594cbf322dba6d637afe41eeb9e9841d079e107
billaud <billaudwilliam@gmail.com>
2017-04-04 22:44:11 +0200

    fin reorganisation main

--- main.c
+++ main.c
@@ -45,21 +45,27 @@ void gestionEvenement(EvenementGfx evenement) {
     //position du zoom par defaut
     static unsigned int x_d = 80, y_d = 80;
     static unsigned int zoom_d = 20;
+    //variable utilisé pour joué les coup
     int x, y;
+    //variable utilisé pour stocker les coup du joueur
     static coup coupJoueur;
+    //variable utlilisé pour stocker l'orientation de la piece
     static orientation orientationPiece;
+    //variable utilisé pour stocker le joueur actuelle
     static int joueurActuelle;
+    //variable utilisé pour stocker le niveau de difficulté
     static int niveauDifficulte = 1;
+    //nom des joueurs
     static char nomJ1[15]="William";
     static char nomJ2[15]="Theo";
+    //permet de savoir si une pièce est selectionné
     static bool pieceSelectionne=false;
     switch (evenement) {
         case Initialisation:
             modePleinEcran();
             initPartie(&joueurActuelle);
-            joueurActuelle = 0;
             trouveMeilleurZoom(&x_d, &y_d, &zoom_d);
-            mode = classique;
+            mode = IA;
             activeGestionDeplacementPassifSouris();
             demandeTemporisation(1000);
             break;
@@ -90,7 +96,9 @@ void gestionEvenement(EvenementGfx evenement) {
                     }
                     break;
                 case menu:
+                    break;
                 case victoire:
+                    afficheChaine("PARTIE FINI ",10,largeurFenetre()/3,hauteurFenetre()/2);
                     break;
 
             }
@@ -147,11 +155,7 @@ void gestionEvenement(EvenementGfx evenement) {
                     switch (toucheClavier()) {
                         case ToucheFlecheDroite:
                         case ToucheFlecheBas:
-                            if (orientationPiece == HG) {
-                                orientationPiece = HD;
-                            } else {
-                                orientationPiece += 1;
-                            }
+                                orientationPiece=(orientationPiece+1)%4;
                             break;
 
                         case ToucheFlecheGauche:
@@ -159,7 +163,8 @@ void gestionEvenement(EvenementGfx evenement) {
                             if (orientationPiece == HD) {
                                 orientationPiece = HG;
                             } else {
-                                orientationPiece -= 1;
+                                //(x+3)%4 =(x-1)%4 mais evite le problème des negatifs
+                                orientationPiece=(orientationPiece+3)%4;
                             }
                             break;
 
@@ -182,7 +187,6 @@ void gestionEvenement(EvenementGfx evenement) {
                     switch (etatBoutonSouris()) {
                         case GaucheAppuye:
                             if (mode == classique || (mode == IA && joueurActuelle == 0)) {
-
                                 if(pieceSelectionne && detecteCase(&x, &y, zoom_d, x_d, y_d)==0) {
                                     coupJoueur.orientationPiece = orientationPiece;
                                     coupJoueur.yCoup = (unsigned int) y;
@@ -196,12 +200,8 @@ void gestionEvenement(EvenementGfx evenement) {
                                         joueurActuelle = (joueurActuelle + 1) % 2;
                                         if (ordreJoueurs[1][20] == 20
                                             && ordreJoueurs[0][20] == 20) {
-                                            if (calculScore(0) > calculScore(1)) {
-                                                puts("joueur vert à gagné");
-                                            } else {
-                                                puts("joueur rouge à gagné");
-                                            }
-                                            initPartie(&joueurActuelle);
+                                           joueurActuelle= calculScore(0) > calculScore(1)?0:1;
+                                                mode =victoire;
                                         }
                                     }
                                     pieceSelectionne = false;
@@ -214,27 +214,15 @@ void gestionEvenement(EvenementGfx evenement) {
                         case ScrollUp:
                             if (toucheCtrlAppuyee()) {
                                 changeZoom(&x_d, &y_d, &zoom_d, true);
-                                printf("nouveau zoom : %d, %d :  %d \n", x_d,
-                                       y_d, zoom_d);
-                            } else {
-                                if (orientationPiece == HD) {
-                                    orientationPiece = HG;
-                                } else {
-                                    orientationPiece -= 1;
-                                }
+                            } else if(pieceSelectionne){
+                                orientationPiece=(orientationPiece+1)%4;
                             }
                             break;
                         case ScrollDown:
                             if (toucheCtrlAppuyee()) {
                                 changeZoom(&x_d, &y_d, &zoom_d, false);
-                                printf("nouveau zoom : %d, %d %d \n", x_d, y_d,
-                                       zoom_d);
-                            } else {
-                                if (orientationPiece == HG) {
-                                    orientationPiece = HD;
-                                } else {
-                                    orientationPiece += 1;
-                                }
+                            } else if(pieceSelectionne){
+                                orientationPiece=(orientationPiece-1)%4;
                             }
 
                             break;
@@ -248,9 +236,22 @@ void gestionEvenement(EvenementGfx evenement) {
                             break;
                     }
                     break;
-                case menu:
-                    break;
                 case victoire:
+                    switch (etatBoutonSouris())
+                    {
+
+                        case GaucheAppuye:
+                        case DroiteAppuye:
+                            mode=menu;
+                            break;
+                        case GaucheRelache:break;
+                        case DroiteRelache:break;
+                        case ScrollUp:break;
+                        case ScrollDown:break;
+                        case TmpScroll:break;
+                    }
+                    break;
+                case menu:
                     break;
                 case tmpIA:
                     break;
@@ -258,12 +259,9 @@ void gestionEvenement(EvenementGfx evenement) {
 
             rafraichisFenetre();
             break;
-
         case Souris:
-
             rafraichisFenetre();
             break;
-
         case Inactivite:
             rafraichisFenetre();
             break;
@@ -276,17 +274,14 @@ void gestionEvenement(EvenementGfx evenement) {
                             joueurActuelle = (joueurActuelle + 1) % 2;
                             if (ordreJoueurs[1][20] == 20
                                 && ordreJoueurs[0][20] == 20) {
-                                if (calculScore(0) > calculScore(1)) {
-                                    mode = victoire;
-                                } else {
-                                    mode = victoire;
-                                }
-                                initPartie(&joueurActuelle);
+                                joueurActuelle= calculScore(0) > calculScore(1)?0:1;
+                                mode =victoire;
                             }
                         }
                     } else {
                         mode = menu;
                     }
+                    trouveMeilleurZoom(&x_d, &y_d, &zoom_d);
                     mode = IA;
                 }
             }
